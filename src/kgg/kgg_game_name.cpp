48b95d59435c620d012d1c47fd473a64bc553710
root <kiyum1406@gmail.com>
2026-03-11 15:09:40 +0000

    spawn and update falling rocks

--- src/kgg/kgg_game_name.cpp
+++ src/kgg/kgg_game_name.cpp
@@ -1,6 +1,8 @@
 #include "bn_keypad.h"
 #include "kgg/kgg_game_name.h"
 #include "mj/mj_game_list.h"
+#include "bn_display.h"
+#include "bn_random.h"
 
 
 namespace {
@@ -28,19 +30,39 @@ int kgg_game_name::total_frames() const
 }
 
 mj::game_result kgg_game_name::play([[maybe_unused]] const mj::game_data& data)
+
 {
-    
-
-    _player.update();
-
-    if(bn::keypad::a_pressed())
+    if(_game_over)
     {
-        _victory = true;
+        return mj::game_result();
     }
 
-    if(bn::keypad::b_pressed())
+    // keeps player moving
+    _player.update();
+
+    // time increases by 1
+    _spawn_timer++;
+
+    if(_spawn_timer >= 30) // spawn a rock every half second
     {
-        _victory = false;
+        _spawn_timer = 0;
+
+        if(_rocks.size() < _rocks.max_size())
+        {
+            static bn::random random;
+            // chossing a random left right position at the top  
+            int random_x = random.get_int(-bn::display::width() / 2 + 8,
+                                          bn::display::width() / 2 - 8);
+
+            // creates a rock high up, so it falls down into view                            
+            _rocks.push_back(rock(random_x, -70));
+        }
+    }
+
+    //rock moves downward every frame
+    for(rock& r : _rocks)
+    {
+        r.update();
     }
 
     return mj::game_result();
